e2a0b0ebafd438ca9ab5d559676ab8dce31fc481
maly <mohamed.aly@cern.ch>
2023-07-15 20:34:18 +0200

    skeleton for OOP approach, classes module and logger added

---
+++ PlotClasses.py
@@ -0,0 +1,154 @@
+class CoffeaPlot(object):
+    '''
+    This class can create the figure, the axes, handle Stacks and RatioPlots and Box
+    objects to produce a plot that gets saved to a file.
+    '''
+    pass
+
+class StylableObject(object):
+
+    VALID_STYLE_SETTINGS = ["linestyle", "linewidth", "color", "edgecolor", "markersize", "maker", "fill", "alpha", "hatch"]
+
+    def __init__(self, **styling):
+        for style in styling:
+            if style not in self.VALID_STYLE_SETTINGS:
+                raise ValueError(f"Invalid style setting: {style}, allowed styles are: {self.VALID_STYLE_SETTINGS}")
+        self.styling = styling
+
+    def color(self):
+        return self.styling.get('color', None)
+
+    def edgecolor(self):
+        return self.styling.get('edgecolor', None)
+
+    def linewidth(self):
+        return self.styling.get('linewidth', None)
+
+    def linestyle(self):
+        return self.styling.get('linestyle', None)
+
+    def fill(self):
+        return self.styling.get('fill', None)
+
+    def alpha(self):
+        return self.styling.get('alpha', None)
+
+    def hatch(self):
+        return self.styling.get('hatch', None)
+
+class DistWithUncObjects(object):
+
+    # points can be used for data
+    ALLOW_BAR_TYPES = ["step", "stepfilled", "points"]
+    SUPPORT_BAR_TYPES = ["stepfilled", "points", "step"]
+    ALLOW_ERROR_TYPES = ["none", "stat", "syst", "stat"]
+    SUPPORT_ERROR_TYPES = ["none", "stat"]
+
+    def __init__(self, bar_type, error_type = 'stat'):
+        self.bar_type = bar_type
+        self.error_type = error_type
+
+        if bar_type not in self.ALLOW_BAR_TYPES:
+            raise ValueError(f"Invalid stack type: {bar_type}")
+        if bar_type not in self.SUPPORT_BAR_TYPES:
+            raise NotImplementedError(f"Unsupported stack type: {bar_type}")
+        if error_type not in self.ALLOW_ERROR_TYPES:
+            raise ValueError(f"Invalid stack error type: {error_type}")
+        if error_type not in self.SUPPORT_ERROR_TYPES:
+            raise NotImplementedError(f"Unsupported stack error type: {error_type}")
+
+class RatioPlots(object):
+    '''
+    A colleciton of ratio plots, when one wants multiple ratio panels.
+    '''
+    pass
+
+class RatioPlot(DistWithUncObjects):
+    def __init__(self, ratio_items = [], bar_type = 'step', error_type = 'stat'):
+
+        '''
+        Create a RatioPlot object.
+
+        This class can hold multiple distributions that are plotted in a ratio panel
+        '''
+        self.ratio_items = []
+
+        DistWithUncObjects.__init__(self, bar_type, error_type)
+
+    def append(self, ratio_item):
+        self.ratio_items.append(ratio_item)
+
+
+class RatioItem(StylableObject):
+    def __init__(self, numerator, denominator, bar_type, label = None, error_type = 'stat', **styling):
+
+        '''
+        Create a RatioItem object.
+
+        This class can hold one distribution that is plotted in a ratio panel
+        '''
+        self.numerator = numerator
+        self.denominator = denominator
+        self.label = label
+
+        StylableObject.__init__(self, **styling)
+
+
+class Stack(DistWithUncObjects):
+    '''
+    A COLLECTION OF Stackatinos.
+    '''
+
+    def __init__(self, stackatinos=[], bar_type = 'filled', error_type = 'stat'):
+
+        # List of stackatinos
+        self.stackatinos = stackatinos
+        DistWithUncObjects.__init__(self, bar_type, error_type)
+
+    def append(self, stackatino):
+        self.stackatinos.append(stackatino)
+
+class Stackatino(StylableObject):
+    '''
+    This is a proxy to an item on a stack plot (within a stack). For example this
+    can be a histogram.
+    '''
+    VALID_STYLE_SETTINGS = ["color", "edgecolor", "linewidth", "linestyle", "fill", "alpha", "hatch"]
+
+    def __init__(self, histograms = [], label = None, **styling):
+
+        self.histograms = histograms
+        self.label = label
+
+        StylableObject.__init__(self, **styling)
+
+    def append(self, histogram):
+        self.histograms.append(histogram)
+
+    def __repr__(self):
+        return f"<Stackatino {self.label}: {', '.join([h for h in self.histograms])}>"
+
+
+class SpanBox(StylableObject):
+    '''
+    Can be used to draw blinding boxes or shade areas of plot
+    '''
+    def __init__(self, pos: tuple, **styling):
+        self.pos = pos
+        StylableObject.__init__(self, **styling)
+
+class VSpanBox(SpanBox):
+
+    def __init__(self, pos: tuple, **styling):
+        SpanBox.__init__(self, pos, **styling)
+
+    def draw(self, ax):
+        ax.axvspan(*self.pos, **self.styling)
+
+class VSpanBox(SpanBox):
+
+    def __init__(self, pos: tuple, **styling):
+        SpanBox.__init__(self, pos, **styling)
+
+    def draw(self, ax):
+        ax.axhspan(*self.pos, **self.styling)
--- plotter.py
+++ plotter.py
@@ -6,10 +6,12 @@ from rescalings import *
 from variables import *
 from utils import *
 from plot_utils import *
-
+from PlotClasses import *
+from logger import ColoredLogger as logger
 import os
 import pickle
 import numpy as np
+from collections import defaultdict
 
 # TODO:: Specify type of sample
 # TODO:: Class for Stack and Stackatino (Stack element)
@@ -20,17 +22,25 @@ import numpy as np
 # TODO:: Specify ylabel
 # TODO:: Specify legend location
 # TODO:: Sample categories
+# TODO:: Nominal rescale should be added in __init__ using regex for affected samples and processor should use regex match
 
+log = logger()
 # Stuff user should give me?
 dump_to = f"outputs/"
+
 BLIND_ON = ['tH','tWH']
+log.info(f"Blinding using {BLIND_ON}")
 DATA_NAME = 'Data'
+log.info(f"Assuming data sample is called {DATA_NAME}")
 
 # Stuff i do not need from user
 data_dir = f"{dump_to}/data/"
+log.info(f"Reading plots from {data_dir}")
 plot_dir = f"{dump_to}/plots/"
+log.info(f"Dumping plots to {plot_dir}")
 
-plot_rescales.append(Rescale('ProtNominal', [''], Functor(lambda w: w, ['weights'])))
+
+rescales_list.append(Rescale('ProtNominal', [''], Functor(lambda w: w, ['weights'])))
 
 for (root, _, files) in os.walk(f'{data_dir}', topdown=True):
     for file in files:
@@ -40,7 +50,7 @@ for (root, _, files) in os.walk(f'{data_dir}', topdown=True):
 
 
         with open(f"{data_dir}/{file}", "rb") as f:
-            coffea_out = pickle.load(f)
+            histograms = pickle.load(f)
 
         # ====== Create directories ====== #
         os.makedirs(f"{plot_dir}/Tables/{tree}/",       exist_ok=True)
@@ -52,73 +62,152 @@ for (root, _, files) in os.walk(f'{data_dir}', topdown=True):
 
         # ====== Loop over 1D plots ====== #
 
-        for hist_key, hist in coffea_out.items():
+        stacks = []
+        datamc_ratios = []
+        signif_ratios = []
+        for region in regions_list:
+            log.info(f"Setting up region {region.name}")
+            region_targets_names = region.targets
+
+            for vars_list in plots_to_make:
+                if vars_list.tree != tree: continue
+                if vars_list.dim  != 1:    continue
+                else:   variables_list = vars_list
+
+            for variable in variables_list:
+                log.info(f"Setting up variable {variable.name}")
+                for rescale in rescales_list:
+                    log.info(f"Setting up rescale {rescale.name}")
+
+                    # ============== Create 1x Stack per region, rescaling, variable ============== #
+                    stack = Stack(bar_type = 'stepfilled', error_type = 'stat')
+                    datamc_ratio = RatioPlot(bar_type = 'step', error_type = 'stat')
+                    signif_ratio = RatioPlot(bar_type = 'step', error_type = 'stat')
+
+                    # ============== Group together samples in different ways ============== #
+                    category_to_samples = defaultdict(list)
+
+                    backgrounds = []
+                    signals = []
+                    region_targets = []
+
+                    data = None
+
+                    for sample in samples_list:
+                        # ============== Group samples from same category ============== #
+                        if sample.category is not None:
+                            category_to_samples[sample.category].append(sample)
+                        else:
+                            category_to_samples[sample.name].append(sample)
+
+                        # ============== Group samples by type ============== #
+                        if sample.type == 'BKG':
+                            log.info(f"Adding sample {sample.name} to backgrounds")
+                            backgrounds.append(sample)
+                        elif sample.type == 'SIG':
+                            log.info(f"Adding sample {sample.name} to signals")
+                            signals.append(sample)
+                        elif sample.type == 'DATA':
+                            log.info(f"Settiing sample {sample.name} as data")
+                            if data is None:
+                                data = sample
+                            else:
+                                log.error("More than one data sample found")
+                        else:
+                            log.error("Sample type not recognised", sample.type)
+
+                    # ============== Make a Stackatino for each category ============== #
+                    for category, samples in category_to_samples.items():
+                        log.info(f"Setting up category {category}")
+                        stackatinos = Stackatino(label = category)
+                        for sample in samples:
+                            histogram = histograms[(variable.name, sample.name, region.name, rescale.name)]
+                            stackatinos.append(histogram)
+
+                        stack.append(stackatinos)
+
+                    # ============== Append Stack to list of Stacks ============== #
+                    stacks.append(stack)
+
+        # ====== Loop over stacks ========= #
+        for stack in stacks:
+            pass
 
 
-        histos_1d  = coffea_out['1D']
-        for region_name, hnames_to_histos in histos_1d.items():
-            region = plot_regions.get_region(region_name)
-            if region is None: print("ERROR:: Region not found", region_name); exit(1)
-            region_targets = region.targets
-            if region_targets == []: region_targets = BLIND_ON
-            num_significance_panels = len(region_targets) + 1 # Main plot + 1 per target
 
 
-            for hname, rescalings_to_histos in hnames_to_histos.items():
-                for plots_list in plots_to_make:
-                    if plots_list.tree != tree: continue
-                    if plots_list.dim != 1:     continue
-                    xlabel = plots_list.get_plot(hname).label
 
 
-                for rescale_name, samples_to_histos in rescalings_to_histos.items():
-                    rescale = plot_rescales.get_rescale(rescale_name)
-                    if rescale is None: print("ERROR:: Rescale not found", rescale_name); exit(1)
 
-                    # ====== Update samples for all histograms to use lateset colors and labels ====== #
-                    for sample_name, histo in samples_to_histos.items():
-                        if sample_name == 'total': continue
-                        sample = plot_samples.get_sample(sample_name)
-                        if sample is None : print("ERROR:: Sample not found", sample_name); exit(1)
 
-                        histo.sample = sample
 
-                    signal_for_blinding       = sum(samples_to_histos[blinding_signal].h for blinding_signal in BLIND_ON)
-                    backgrounds_for_blinding  = sum([histo.h for sample, histo in samples_to_histos.items() if sample not in BLIND_ON and sample != DATA_NAME and sample != 'total'])
-                    blind_bool                = get_blinding(signal_for_blinding, backgrounds_for_blinding)
 
-                    # ====== Make stacked histogram with significance per bin in secondary panels ====== #
 
-                    signif_fig, signif_main_ax, signif_axs = create_fig_with_n_panels(1, num_significance_panels)
 
-                    data_h = samples_to_histos[DATA_NAME]
+        # histos_1d  = coffea_out['1D']
+        # for region_name, hnames_to_histos in histos_1d.items():
+        #     region = plot_regions.get_region(region_name)
+        #     if region is None: print("ERROR:: Region not found", region_name); exit(1)
+        #     region_targets = region.targets
+        #     if region_targets == []: region_targets = BLIND_ON
+        #     num_significance_panels = len(region_targets) + 1 # Main plot + 1 per target
 
-                    to_stack = []
-                    for sample_name, histo in samples_to_histos.items():
-                        histo.set_label(xlabel)
-                        if sample_name == DATA_NAME: continue
-                        if sample_name == 'total': continue
-                        to_stack.append(histo)
 
-                    plot_stack(to_stack, data_h, signif_main_ax, blind_bool, title = f'Region = {region.name} and Rescaling = {rescale.name}')
+        #     for hname, rescalings_to_histos in hnames_to_histos.items():
+        #         for plots_list in plots_to_make:
+        #             if plots_list.tree != tree: continue
+        #             if plots_list.dim != 1:     continue
+        #             xlabel = plots_list.get_plot(hname).label
 
-                    for i, target in enumerate(region_targets):
-                        is_last_target = (i == len(signif_axs) - 1)
-                        target_histo = samples_to_histos[target]
 
-                        non_target_histo  = sum([histo.h for sample, histo in samples_to_histos.items() if sample != target and sample != DATA_NAME])
+        #         for rescale_name, samples_to_histos in rescalings_to_histos.items():
+        #             rescale = plot_rescales.get_rescale(rescale_name)
+        #             if rescale is None: print("ERROR:: Rescale not found", rescale_name); exit(1)
 
-                        # We compute signficance per bin and plot it
-                        signif, signif_err = get_signif_per_bin(target_histo.h, non_target_histo)
-                        plot_signif_per_bin(target_histo, signif, signif_err, signif_axs[i], is_last_target)
+        #             # ====== Update samples for all histograms to use lateset colors and labels ====== #
+        #             for sample_name, histo in samples_to_histos.items():
+        #                 if sample_name == 'total': continue
+        #                 sample = plot_samples.get_sample(sample_name)
+        #                 if sample is None : print("ERROR:: Sample not found", sample_name); exit(1)
 
-                    signif_fig.savefig(f"{plot_dir}/Significance/{tree}/{region.name}_{hname}_{rescale_name}.pdf", bbox_inches="tight")
+        #                 histo.sample = sample
 
-                    plt.clf()
-                    plt.close('all')
+        #             signal_for_blinding       = sum(samples_to_histos[blinding_signal].h for blinding_signal in BLIND_ON)
+        #             backgrounds_for_blinding  = sum([histo.h for sample, histo in samples_to_histos.items() if sample not in BLIND_ON and sample != DATA_NAME and sample != 'total'])
+        #             blind_bool                = get_blinding(signal_for_blinding, backgrounds_for_blinding)
 
-                    # ====== Make stacked histogram with data/mc ratio per bin in secondary panels ====== #
-                    datamc_fig, datamc_main_ax, datamc_ax  = create_fig_with_n_panels(1, 2, h_ratio=[1,0.2])
+        #             # ====== Make stacked histogram with significance per bin in secondary panels ====== #
+
+        #             signif_fig, signif_main_ax, signif_axs = create_fig_with_n_panels(1, num_significance_panels)
+
+        #             data_h = samples_to_histos[DATA_NAME]
+
+        #             to_stack = []
+        #             for sample_name, histo in samples_to_histos.items():
+        #                 histo.set_label(xlabel)
+        #                 if sample_name == DATA_NAME: continue
+        #                 if sample_name == 'total': continue
+        #                 to_stack.append(histo)
+
+        #             plot_stack(to_stack, data_h, signif_main_ax, blind_bool, title = f'Region = {region.name} and Rescaling = {rescale.name}')
+
+        #             for i, target in enumerate(region_targets):
+        #                 is_last_target = (i == len(signif_axs) - 1)
+        #                 target_histo = samples_to_histos[target]
+
+        #                 non_target_histo  = sum([histo.h for sample, histo in samples_to_histos.items() if sample != target and sample != DATA_NAME])
+
+        #                 # We compute signficance per bin and plot it
+        #                 signif, signif_err = get_signif_per_bin(target_histo.h, non_target_histo)
+        #                 plot_signif_per_bin(target_histo, signif, signif_err, signif_axs[i], is_last_target)
+
+        #             signif_fig.savefig(f"{plot_dir}/Significance/{tree}/{region.name}_{hname}_{rescale_name}.pdf", bbox_inches="tight")
+
+        #             plt.clf()
+        #             plt.close('all')
+
+        #             # ====== Make stacked histogram with data/mc ratio per bin in secondary panels ====== #
+        #             datamc_fig, datamc_main_ax, datamc_ax  = create_fig_with_n_panels(1, 2, h_ratio=[1,0.2])
 
                     # for sample_name, dim_to_histos in samples_to_histos.items():
                     #     histos_1d = dim_to_histos['1D']
