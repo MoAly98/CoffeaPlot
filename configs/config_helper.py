1a1ba75f887f55a9a45434a4258b6cb1f0c600ef
maly <mohamed.aly@cern.ch>
2023-09-11 13:06:10 +0200

    config update

--- configs/config_helper.py
+++ configs/config_helper.py
@@ -11,7 +11,7 @@ def MM_weight(mm_weight):
 # ==== Variables ==== #
 
 bdt_tH  =  lambda x: x[:,0]
-bdt_ttb = lambda x: x[:,1]
+bdt_ttb =   lambda x: x[:,1]
 nlights = lambda njets, nbjets: njets - nbjets
 tagnonb_topb_m = lambda tagnonb_topb_m: tagnonb_topb_m/1e3
 
@@ -22,19 +22,23 @@ xxx_is_c     = lambda HFClass: HFClass == -1
 xxx_is_b     = lambda HFClass: HFClass == 1
 xxx_is_light = lambda HFClass: HFClass == 0
 xxx_is_2b    = lambda HFClass: ( (abs(HFClass) >= 200) & (abs(HFClass) < 1000) ) | ( (abs(HFClass) >= 1100) & (abs(HFClass) < 2000) ) | (abs(HFClass) >= 2000)
-xxx_is_1b    = lambda HFClass: ( (abs(HFClass) >= 1000) & (abs(HFClass) < 1100) ) | ( (abs(HFClass) >= 100) & (abs(HFClass) < 200) )
+xxx_is_1b    = lambda HFClass: ( (abs(HFClass) >= 1000) & (abs(HFClass) < 1100) )
+xxx_is_1B    = lambda HFClass: ( (abs(HFClass) >= 100)  & (abs(HFClass) < 200) )
+xxx_is_1bB    = lambda HFClass: (( xxx_is_1b(HFClass) ) | ( xxx_is_1B(HFClass) ))
 
-ttb_cut = lambda x, y: (tight_lepton(x)) & (xxx_is_b(y))
-ttc_cut = lambda x, y: (tight_lepton(x)) & (xxx_is_c(y))
-ttl_cut = lambda x, y: (tight_lepton(x)) & (xxx_is_light(y))
-ttbb_cut = lambda x, y: (tight_lepton(x)) & (xxx_is_2b(y))
+ttb_cut   = lambda x, y: (tight_lepton(x)) & (xxx_is_b(y))
+ttc_cut   = lambda x, y: (tight_lepton(x)) & (xxx_is_c(y))
+ttl_cut   = lambda x, y: (tight_lepton(x)) & (xxx_is_light(y))
+ttbb_cut  = lambda x, y: (tight_lepton(x)) & (xxx_is_2b(y))
 tt1b_cut = lambda x, y: (tight_lepton(x)) & (xxx_is_1b(y))
+tt1B_cut = lambda x, y: (tight_lepton(x)) & (xxx_is_1B(y))
+tt1bB_cut = lambda x, y: (tight_lepton(x)) & (xxx_is_1bB(y))
 
 # ===== Regions ===== #
 # PRESEL
 PR_fn     =    lambda njets, nbjets, njets_CBT4, njets_CBT5, nalljets, njets_CBT0, tau_pt, nfwd:  (~((njets>=5) & (nbjets>=4))) & (ak.num(tau_pt, axis=1) == 0)
 # SIGNAL REGIONS
-SR_fn     =    lambda njets, nbjets, njets_CBT4, njets_CBT5, nalljets, njets_CBT0, tau_pt, nfwd:  (~((njets>=5) & (nbjets>=4))) & (ak.num(tau_pt, axis=1) == 0) & ((njets_CBT4+njets_CBT5) >= 3) & ((nalljets - njets_CBT5 - njets_CBT4 - njets_CBT0) < 2) & (nfwd>0)
+SR_fn     =    lambda njets, nbjets, njets_CBT4, njets_CBT5, nalljets, njets_CBT0, tau_pt, nfwd:  (~((njets>=5) & (nbjets>=4))) & (ak.num(tau_pt, axis=1) == 0) & ((njets_CBT4+njets_CBT5) >= 3) & ((nalljets - njets_CBT5 - njets_CBT4 - njets_CBT0) <  2) & (nfwd>0)
 # TTB CR REGIONS
 CR_ttb_fn =    lambda njets, nbjets, njets_CBT4, njets_CBT5, nalljets, njets_CBT0, tau_pt, nfwd:  (~((njets>=5) & (nbjets>=4))) & (ak.num(tau_pt, axis=1) == 0) & ((njets_CBT4+njets_CBT5) >= 3) & ((nalljets - njets_CBT5 - njets_CBT4 - njets_CBT0) >= 2) & (nfwd==0)
 
